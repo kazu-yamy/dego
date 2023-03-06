5771a8c86af09203e2cf6d5857742791ffe599e9
macha <kazuki.aishima.dev@gmail.com>
2023-03-06 11:32:02 +0900

    create splash screen

--- lib/views/base_tab_view.dart
+++ lib/views/base_tab_view.dart
@@ -14,6 +14,31 @@ class BaseTabView extends ConsumerWidget {
     AppPage(),
   ];
 
+  Widget buttomNav(BuildContext context, WidgetRef ref) {
+    final view = ref.watch(baseTabViewProvider);
+    return BottomNavigationBar(
+      items: [
+        BottomNavigationBarItem(
+            icon: const Text("IP"),
+            label: 'ipAddress',
+            backgroundColor: Colors.deepPurpleAccent.computeLuminance() > 0.5
+                ? Colors.black
+                : Colors.white),
+        BottomNavigationBarItem(
+            icon: const Icon(Icons.home),
+            label: 'home',
+            backgroundColor: Colors.deepPurpleAccent.computeLuminance() > 0.5
+                ? Colors.black
+                : Colors.white),
+      ],
+      currentIndex: view.index,
+      onTap: (int index) =>
+          ref.read(baseTabViewProvider.notifier).state = ViewType.values[index],
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: Colors.deepPurpleAccent,
+    );
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final view = ref.watch(baseTabViewProvider);
@@ -21,28 +46,7 @@ class BaseTabView extends ConsumerWidget {
     return Scaffold(
       appBar: AppBar(title: const Text("Dego")),
       body: widgets[view.index],
-      bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(
-                icon: const Text("IP"),
-                label: 'ipAddress',
-                backgroundColor:
-                    Colors.deepPurpleAccent.computeLuminance() > 0.5
-                        ? Colors.black
-                        : Colors.white),
-            BottomNavigationBarItem(
-                icon: const Icon(Icons.home),
-                label: 'home',
-                backgroundColor:
-                    Colors.deepPurpleAccent.computeLuminance() > 0.5
-                        ? Colors.black
-                        : Colors.white),
-          ],
-          currentIndex: view.index,
-          onTap: (int index) => ref.read(baseTabViewProvider.notifier).state =
-              ViewType.values[index],
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: Colors.deepPurpleAccent),
+      bottomNavigationBar: buttomNav(context, ref),
     );
   }
 }
